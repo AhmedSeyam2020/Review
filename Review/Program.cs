8d73fe086accf13ff10d9cc7bf5ba14cd0796cd4
Ahmed <Ahmed@DESKTOP-CR8UIMM>
2020-10-25 21:29:50 +0200

    version 2

--- Review/Program.cs
+++ Review/Program.cs
@@ -7,6 +7,8 @@ namespace Review
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World! ahmed");
+            Console.WriteLine("Hello World! ahmed");
         }
+    }
     }
 }
